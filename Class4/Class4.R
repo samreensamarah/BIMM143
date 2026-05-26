d0783af5b6f3a0c5218e728e20adfeae9d03bb3f
Samarah Zaman <samreensamarah@Samreens-MacBook-Air.local>
2026-05-26 12:36:38 -0700

    files

---
+++ Class4/Class4.R
@@ -0,0 +1,9 @@
+#' ---
+#' title: "Class 4: Intro to R"
+#' author: "Samarah Zaman (PID:A17411141)"
+#' date: "04/09/2026"
+#' ---
+#This is just a text file
+x <- 1: 50
+plot(x, sin(x), type="l", col="red", lwd=3)
+
